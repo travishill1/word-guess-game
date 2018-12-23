2257bfe5c4934261d160f484efa029ebdfb09bee
Konig <travishill1@gmail.com>
2018-12-22 22:54:44 -0600

    some more code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,9 @@
     var foundCount = 0;
 
     var currentWord = ["impeach", "emolument", "collusion", "scandal","fraud"];
+
+    var audioHorn = document.createElement("audio");
+    audioHorn.setAttribute("src", "assets/horn.mp3");
   
     const spacing = " ";
     
@@ -31,8 +34,7 @@
       for(var i = 0; i < currentWord.length; i++){
 
         if(userInput === currentWord[i]){
-          
-       
+              
           containers[i].textContent = userInput;
           
           found = true;
@@ -45,20 +47,25 @@
       }
       
       // if not found then adds incorrect letter on display
+      
       if(!found) { incorrectLetters.innerHTML = incorrectLetters.innerHTML + userInput; }
-    }
-    // };
+      // function() {
+      //   audioHorn.play();
+      };
   
     // for selecting words in our array at random
     function randomSelector(){return Math.floor(Math.random() * currentWord.length) + 1; 
     }
-  
+
+    console.log(currentWord[randomSelector()])
+    console.log(currentWord.length)
+    currentWordLength = currentWord[randomSelector()].length
 
     for(var j = 0; j < currentWord.length; j++){
      if(j == randomSelector){
       // = currentWord[j]
       let currentWord2 = currentWord[j];
-      $("#wins").append(currentWord2)
+      $("#guesses").append(currentWord2)
     // change #wins to preferred div
     // want another for loop within this for loop to output the spaces for each word.
      }
